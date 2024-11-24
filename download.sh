b00bf7dcccc34fa092221eca8d2006ab4c0f7a40
malinka <70282392+malinkaphann@users.noreply.github.com>
2024-11-24 23:42:03 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # install system image for android 30
-sdkmanager "system-images;android-30;google_apis;x86_64"
+sdkmanager "system-images;android-35;google_apis_playstore;x86_64"
 
 # install platforms for android 30
-sdkmanager "platforms;android-30"
+sdkmanager "platforms;android-35"
 
 # install platform tools
 sdkmanager "platform-tools"
@@ -13,7 +13,7 @@ sdkmanager "platform-tools"
 sdkmanager "ndk-bundle"
 
 # install build tools
-sdkmanager "build-tools;30.0.1"
+sdkmanager "build-tools;35.0.0"
 
 # install emulator
 sdkmanager "emulator"
